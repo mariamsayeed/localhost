335a2888f5a003f4e8f345e221ff7d821b57a715
Mariam Sayeed <marve7530@gmail.com>
2023-09-18 10:49:15 +0530

    ongoing project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'; 
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Recommend from './components/Recommend';
+import ScrollToTop from './components/ScrollToTop';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+
+function App() {
+  return (
+    <>
+     <ScrollToTop />
+    <Navbar />
+    <Hero />
+    <Services />
+    <Recommend />
+    <Testimonials />
+    <Footer />
+  </>
+  );
+}
+
+export default App;
